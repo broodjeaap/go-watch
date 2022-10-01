9def3ca2d5b279ce6625f94e684088af462dcc9b
BroodjeAap <david@broodjeaap.net>
2022-10-01 12:01:12 +0000

    removed debug print

--- scraping.go
+++ scraping.go
@@ -414,7 +414,6 @@ func roundFloat(val float64, precision uint) float64 {
 	}
 	ratio := math.Pow(10, float64(precision))
 	rounded := math.Round(val*ratio) / ratio
-	log.Println(val, precision, ratio, rounded)
 	return rounded
 }
 
