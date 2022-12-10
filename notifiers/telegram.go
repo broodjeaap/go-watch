0b961718b82c52ce67632ba98128a79f4e7f7389
BroodjeAap <david@broodjeaap.net>
2022-12-10 11:23:03 +0000

    removed debug prints

--- notifiers/telegram.go
+++ notifiers/telegram.go
@@ -26,11 +26,7 @@ func (telegram *TelegramNotifier) Open() bool {
 }
 
 func (telegram *TelegramNotifier) Message(message string) bool {
-	log.Println(telegram)
-	log.Println(message)
 	msg := tgbotapi.NewMessage(viper.GetInt64("telegram.chat"), message)
-	log.Println(msg)
-	log.Println(telegram.Bot)
 	_, err := telegram.Bot.Send(msg)
 	if err != nil {
 		log.Println("Could not send Telegram message:\n", err)
