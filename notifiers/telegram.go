23bb6459ff763e19faaacb6e593a89a0c5f05940
BroodjeAap <david@broodjeaap.net>
2022-12-26 15:22:21 +0000

    moved notifiers to seperate section in config

--- notifiers/telegram.go
+++ notifiers/telegram.go
@@ -14,19 +14,19 @@ type TelegramNotifier struct {
 }
 
 func (telegram *TelegramNotifier) Open() bool {
-	bot, err := tgbotapi.NewBotAPI(viper.GetString("telegram.token"))
+	bot, err := tgbotapi.NewBotAPI(viper.GetString("notifiers.telegram.token"))
 	if err != nil {
 		log.Println("Could not start Telegram notifier:\n", err)
 		return false
 	}
 	telegram.Bot = bot
-	bot.Debug = viper.GetBool("telegram.debug")
+	bot.Debug = viper.GetBool("notifiers.telegram.debug")
 	log.Printf("Authorized telegram bot: %s", bot.Self.UserName)
 	return true
 }
 
 func (telegram *TelegramNotifier) Message(message string) bool {
-	msg := tgbotapi.NewMessage(viper.GetInt64("telegram.chat"), message)
+	msg := tgbotapi.NewMessage(viper.GetInt64("notifiers.telegram.chat"), message)
 	_, err := telegram.Bot.Send(msg)
 	if err != nil {
 		log.Println("Could not send Telegram message:\n", err)
